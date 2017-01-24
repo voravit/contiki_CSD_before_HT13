ccf63a1928fd6efa8f38ed7e73257d52fa5c8027
Peter Sjodin <psj@kth.se>
2017-01-24 22:52:25 +0100

    Support for I2C communication in PMS5003 family sensors.

--- platform/avr-rss2/dev/pms5003.c
+++ platform/avr-rss2/dev/pms5003.c
@@ -59,10 +59,11 @@
 /* Valid values for body length field */
 #define PMSMINBODYLEN 20
 #define PMSMAXBODYLEN 28
-/* Buffer holds message body plus length field (two bytes) */
-#define PMSBUFFER (PMSMAXBODYLEN+2)
+/* Buffer holds frame body plus preamble (two bytes)
+ * and length field (two bytes) */
+#define PMSBUFFER (PMSMAXBODYLEN+4)
 
-/* Two Preamble bytes */
+/* Two preamble bytes */
 #define PRE1 0x42
 #define PRE2 0x4d
 
@@ -71,14 +72,14 @@ static uint16_t PM1, PM2_5, PM10;
 static uint16_t PM1_ATM, PM2_5_ATM, PM10_ATM;
 static uint32_t invalid_frames, valid_frames;
 /*---------------------------------------------------------------------------*/
-PROCESS(pms5003_process, "PMS5003/UART dust sensor process");
+PROCESS(pms5003_uart_process, "PMS5003/UART dust sensor process");
 PROCESS(pms5003_i2c_process, "PMS5003/I2C dust sensor process");
- /* AUTOSTART_PROCESSES(&start_process, &pms5003_process);*/
 /*---------------------------------------------------------------------------*/
 void pms5003_init() {
   rs232_set_input(RS232_PORT_0, serial_raw_input_byte);
   serial_raw_init();
-  process_start(&pms5003_process, NULL);
+  pms5003_event = process_alloc_event();
+  process_start(&pms5003_uart_process, NULL);
   process_start(&pms5003_i2c_process, NULL);
   return;
 }
@@ -125,32 +126,64 @@ uint32_t pms5003_invalid_frames() {
 static int check_pmsframe(uint8_t *buf) {
   int sum, pmssum;
   int i;
-  int len = (buf[0] << 8) + buf[1];
-  
+  int len;
+
+  if (buf[0] != PRE1 || buf[1] != PRE2)
+    return 0;
+  /* len is length of frame not including preamble and checksum */
+  len = (buf[2] << 8) + buf[3];
   if (len < PMSMINBODYLEN || len > PMSMAXBODYLEN) {
     return 0;
   }
-  /* Compute checksum */
-  sum = PRE1+PRE2; /* Fixed preamble */
-  /* Sum data bytewise, excluding checksum field */
-  for (i = 0; i < len; i++) {
+  /* Sum data bytewise, including preamble but excluding checksum */
+  sum = 0;
+  for (i = 0; i < len+2; i++) {
     sum += buf[i];
   }
-  /* Compare with received checksum */
-  pmssum = (buf[len] << 8) + buf[len+1];
+  /* Compare with received checksum last in frame*/
+  pmssum = (buf[len+2] << 8) + buf[len+3];
   return (pmssum == sum);
 }
 /*---------------------------------------------------------------------------*/
+/* Frame received from PMS sensor. Validate and update sensor data. 
+ * Return 1 if valid frame, otherwise 0 
+ */
+static int
+pmsframe(uint8_t *buf) {
+
+  if (check_pmsframe(buf)) {
+    valid_frames++;
+    /* Update sensor readings */
+    PM1 = (buf[4] << 8) | buf[5];
+    PM2_5 = (buf[6] << 8) | buf[7];
+    PM10 = (buf[8] << 8) | buf[9];      
+    PM1_ATM = (buf[10] << 8) | buf[11];
+    PM2_5_ATM = (buf[12] << 8) | buf[13];
+    PM10_ATM = (buf[14] << 8) | buf[15];      
+    return 1;
+  }
+  else {
+    invalid_frames++;
+    return 0;
+  }
+}
+
+static void
+printpm() {
+    printf("PMS frames: valid %lu, invalid %lu\n", valid_frames, invalid_frames);
+    printf("PM1 = %04d, PM2.5 = %04d, PM10 = %04d\n", PM1, PM2_5, PM10);
+    printf("PM1_ATM = %04d, PM2.5_ATM = %04d, PM10_ATM = %04d\n", PM1_ATM, PM2_5_ATM, PM10_ATM);	
+}
+  
+/*---------------------------------------------------------------------------*/
 extern process_event_t serial_raw_event_message;
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(pms5003_process, ev, data)
+PROCESS_THREAD(pms5003_uart_process, ev, data)
 {
   static uint8_t buf[PMSBUFFER], *bufp;
   static int remain;
 
   PROCESS_BEGIN();
-
-  pms5003_event = process_alloc_event();
 
   while(1) {
     /* First check for two bytes of fixed preamble  */
@@ -189,40 +222,13 @@ PROCESS_THREAD(pms5003_process, ev, data)
 	} while (ev != serial_raw_event_message);
 	*bufp++ = *((uint8_t *) data);
       }		   
-      /* Verify that is is a valid frame */
-      if (check_pmsframe(buf)) {
-	valid_frames++;
-	/* Update sensor readings */
-	PM1 = (buf[2] << 8) | buf[3];
-	PM2_5 = (buf[4] << 8) | buf[5];
-	PM10 = (buf[6] << 8) | buf[7];      
-	PM1_ATM = (buf[8] << 8) | buf[9];
-	PM2_5_ATM = (buf[10] << 8) | buf[11];
-	PM10_ATM = (buf[12] << 8) | buf[13];      
-#if 0
-	printf("valid %lu, invalid %lu: ", valid_frames, invalid_frames);
-	printf("PM1 = %04d, PM2.5 = %04d, PM10 = %04d\n", PM1, PM2_5, PM10);
-	printf("PM1_ATM = %04d, PM2.5_ATM = %04d, PM10_ATM = %04d\n", PM1_ATM, PM2_5_ATM, PM10_ATM);	
-#endif
+      if (pmsframe(buf)) {
 	/* Tell other processes there is new data */
 	if (process_post(PROCESS_BROADCAST, pms5003_event, NULL) == PROCESS_ERR_OK) {
 	  PROCESS_WAIT_EVENT_UNTIL(ev == pms5003_event);
 	}
+	
       }
-      else {
-	invalid_frames++;
-#if 0
-	printf("Frame validation failed\n");
-#endif
-      }
-#if 1
-      if (((invalid_frames + valid_frames) % 10) == 0) {
-	printf("valid %lu, invalid %lu: ", valid_frames, invalid_frames);
-	printf("PM1 = %04d, PM2.5 = %04d, PM10 = %04d\n", PM1, PM2_5, PM10);
-	printf("PM1_ATM = %04d, PM2.5_ATM = %04d, PM10_ATM = %04d\n", PM1_ATM, PM2_5_ATM, PM10_ATM);	
-      }
-#endif
-
     }
   }
   PROCESS_END();
@@ -233,11 +239,11 @@ static struct etimer i2ctimer;
 
 PROCESS_THREAD(pms5003_i2c_process, ev, data)
 {
-
+  static uint8_t buf[PMSBUFFER];
   extern uint16_t i2c_probed; /* contiki_main.c: i2c devices we have probed */
 
   PROCESS_BEGIN();
-  etimer_set(&i2ctimer, CLOCK_SECOND*30);
+  etimer_set(&i2ctimer, CLOCK_SECOND*15);
 
   /* Main loop */
   while(1) {
@@ -245,10 +251,17 @@ PROCESS_THREAD(pms5003_i2c_process, ev, data)
     PROCESS_YIELD();
 
     if((ev == PROCESS_EVENT_TIMER) && (data == &i2ctimer)) {
-	    if (i2c_probed & I2C_PMS5003)
-		    printf("probed I2C\n");
-	    else
-		    printf("Not probbed I2C\n");
+      if (i2c_probed & I2C_PMS5003) {
+	i2c_read_mem(I2C_PMS5003_ADDR, 0, buf, PMSBUFFER);
+	if (pmsframe(buf)) {
+	  printpm();
+	  /* Tell other processes there is new data */
+	  if (process_post(PROCESS_BROADCAST, pms5003_event, NULL) == PROCESS_ERR_OK) {
+	    PROCESS_WAIT_EVENT_UNTIL(ev == pms5003_event);
+	  }
+	}
+      }
+      etimer_reset(&i2ctimer);
     }
   }
   PROCESS_END();
